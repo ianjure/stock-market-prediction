1d08a91b4183c7cb21e6844b0d76d63c2c1f5eb6
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-08-29 07:57:13 +0800

    Add files via upload

--- validate.py
+++ validate.py
@@ -8,12 +8,18 @@ with open('stocks_raw.txt', 'r') as file:
 
 new_list = []
 for ticker in symbols_list:
-    try:
-        com_name = yf.Ticker(ticker).info['shortName']
-        com_name = com_name.replace("-", "").replace(".", "")
-        new_list.append(f"{ticker} - {com_name}")
-    except:
-        continue
+    stock = yf.Ticker(ticker)
+    stock_hist = stock.history(period="max")
+    if stock_hist.shape[0] > 1050:
+        try:
+            stock_website = stock.info['website']
+            stock_sector = stock.info['sector']
+            stock_industry = stock.info['industry']
+            com_name = stock.info['shortName']
+            com_name = com_name.replace("-", "").replace(".", "")
+            new_list.append(f"{ticker} - {com_name}")
+        except:
+            continue
 
 list_str = "|".join(new_list)
 
