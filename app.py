beb2e51c9cc86908663932a1e9c76223e58a1c85
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-08-28 13:44:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from preprocess import format_week, format_month
 from backtest import backtest
 
 # PAGE CONFIGURATIONS
-st.set_page_config(page_title="🕵️ Stock Trend Forecaster", page_icon="🏛️", layout="centered", initial_sidebar_state="collapsed")
+st.set_page_config(page_title="Stock Trend Forecaster", page_icon="🏛️", layout="centered", initial_sidebar_state="collapsed")
 
 top = """
         <style>
@@ -28,7 +28,7 @@ hide = """
 st.markdown(hide, unsafe_allow_html=True)
 
 # TITLE
-st.markdown("<h1 style='text-align: center;'>Stock Trend Forecaster</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>🕵️ Stock Trend Forecaster</h1>", unsafe_allow_html=True)
 
 # SUBTITLE
 st.markdown("<h6 style='text-align: center;'>Predicts market movements based on historical data and economic indicators.</h6>", unsafe_allow_html=True)
