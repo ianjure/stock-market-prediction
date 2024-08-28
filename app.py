7c811e5c7d24c82349ad9b07bc58b36ec1bf723b
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-08-28 14:08:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown(hide, unsafe_allow_html=True)
 
 # TITLE
 st.markdown("<p style='text-align: center; font-size: 6rem; line-height: 0.4;'>🕵️</p>", unsafe_allow_html=True)
-st.markdown("<p style='text-align: center; font-size: 2.8rem; font-weight: 800; line-height: 0.8; text-shadow: 2px 2px 4px #000000;'>Stock Trend Forecaster</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; font-size: 2.8rem; font-weight: 800; line-height: 0.8; text-shadow: 0px -5px 10px #000000;'>Stock Trend Forecaster</p>", unsafe_allow_html=True)
 
 # SUBTITLE
 st.markdown("<p style='text-align: center; font-size: 1rem; font-weight: 500; line-height: 1.2;'>Predicts market movements based on historical and economic data.</p>", unsafe_allow_html=True)
